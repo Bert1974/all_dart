586c065c2b27accad8429b5d95d055fb11294271
Bert Bruggeman <bbruggeman1974@gmail.com>
2023-04-16 15:45:57 +0200

    config.json for server

--- server/lib/server.dart
+++ server/lib/server.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -9,11 +10,13 @@ import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:data/data.dart';
 import 'package:server/src/localization/servermessages.i18n.dart';
 
-const secretPassphrase = '123secretjwt';
-const String databaseDirectory = "database";
-const serverPort = 2222;
-const serverUrl = "192.168.178.15";
-const webdir="../publish/web/";
+const configFileName = 'config.json';
+
+late final String secretPassphrase;
+late final String databaseDirectory;
+late final int serverPort;
+late final  String serverUrl;
+late final String webdir;
 
 response(String message, dynamic data, bool success) {
   return <String, dynamic>{
@@ -118,77 +121,117 @@ extension AlfredExtension on Alfred {
   }
 }
 
+Future<String?> checkDir(String? path) async {
+  if (path != null && path.isNotEmpty) {
+    return await Directory(path).exists() ? path : null;
+  }
+  return null;
+}
+
+Future<bool> loadConfig() async {
+  final configFile = File(configFileName);
+  final jsonString = await configFile.readAsString();
+  final dynamic settings = jsonDecode(jsonString);
+
+  var dir = await checkDir(settings['webDirectory']);
+  if (dir == null) {
+    return false;
+  }
+  webdir = dir;
+  dir = await checkDir(settings['databaseDirectory']);
+  if (dir == null) {
+    return false;
+  }
+  databaseDirectory = dir;
+  var jwt = settings['JWTSecret'];
+  if (jwt == null || jwt.isEmpty) {
+    return false;
+  }
+  secretPassphrase = jwt;
+
+  if (settings['serverPort'] is! int) {
+    return false;
+  }
+  serverPort = settings['serverPort'];
+  serverUrl = settings['serverUrl'];
+  return true;
+}
+
 void startServer(data) async {
-  //SendPort sendPort = data[0];
-  ByteData reference = data[0];
+  if (await loadConfig()) {
+    //SendPort sendPort = data[0];
+    ByteData reference = data[0];
 
-  connection = Database.openStore(databaseDirectory)!;
-  connection.setReference(reference);
+    connection = Database.openStore(databaseDirectory)!;
+    connection.setReference(reference);
 
-  final app = Alfred(onNotFound: (req, res) {
-    return File('${webdir}index.html');
-  });
+    final app = Alfred(onNotFound: (req, res) {
+      return File('${webdir}index.html');
+    });
 
-  app.all('*', cors(/*origin: '127.0.0.1:2222')*/));
+    app.all('*', cors(/*origin: '127.0.0.1:2222')*/));
 
-  app.auth('/api/user/setting', (req, res, connection, user) async {
-    final body = (await req.body) as Map<String, dynamic>;
-    await connection.saveUserSettings(user, body['type'], body['data']);
-    return responseOk();
-  });
-  app.auth('/api/servers', (req, res, connection, user) async {
-    return responseResult<List<Server>>(await connection.getServers(user),
-        (List<Server> data) => data.map((s) => s.toJson() as dynamic).toList());
-  });
-  app.auth('/api/save_server', (req, res, connection, user) async {
-    final body = (await req.body) as Map<String, dynamic>;
-    return responseResult<bool>(
-        await connection.saveServer(user, Server.fromJson(body)),
-        (bool data) => data);
-  });
-  app.auth('/api/currentuser', (req, res, connection, user) async {
-    return responseData(<String, dynamic>{'user': user});
-  });
-  app.post('/api/login', (req, res) async {
-    try {
-      var connection = getConnection(req);
+    app.auth('/api/user/setting', (req, res, connection, user) async {
       final body = (await req.body) as Map<String, dynamic>;
-      Result<User> user =
-          await connection.login(body['name'], body['password']);
-      if (user.result != null) {
-        // Create a json web token
+      await connection.saveUserSettings(user, body['type'], body['data']);
+      return responseOk();
+    });
+    app.auth('/api/servers', (req, res, connection, user) async {
+      return responseResult<List<Server>>(
+          await connection.getServers(user),
+          (List<Server> data) =>
+              data.map((s) => s.toJson() as dynamic).toList());
+    });
+    app.auth('/api/save_server', (req, res, connection, user) async {
+      final body = (await req.body) as Map<String, dynamic>;
+      return responseResult<bool>(
+          await connection.saveServer(user, Server.fromJson(body)),
+          (bool data) => data);
+    });
+    app.auth('/api/currentuser', (req, res, connection, user) async {
+      return responseData(<String, dynamic>{'user': user});
+    });
+    app.post('/api/login', (req, res) async {
+      try {
+        var connection = getConnection(req);
+        final body = (await req.body) as Map<String, dynamic>;
+        Result<User> user =
+            await connection.login(body['name'], body['password']);
+        if (user.result != null) {
+          // Create a json web token
 // Pass the payload to be sent in the form of a map
-        final jwt = JWT(
-          // Payload
-          {
-            'id': user.result!.name,
-          },
+          final jwt = JWT(
+            // Payload
+            {
+              'id': user.result!.name,
+            },
 
-          issuer: 'https://git.bertbruggeman.nl',
-        );
+            issuer: 'https://git.bertbruggeman.nl',
+          );
 
-        // Sign it (default with HS256 algorithm)
-        var token = jwt.sign(SecretKey(secretPassphrase),
-            expiresIn: Duration(hours: 2));
-        return responseData(
-            <String, dynamic>{'token': token, 'user': user.result});
+          // Sign it (default with HS256 algorithm)
+          var token = jwt.sign(SecretKey(secretPassphrase),
+              expiresIn: Duration(hours: 2));
+          return responseData(
+              <String, dynamic>{'token': token, 'user': user.result});
+        }
+        return responseError(user.error);
+      } catch (e) {
+        print(e);
+        return responseException(e);
       }
-      return responseError(user.error);
-    } catch (e) {
-      print(e);
-      return responseException(e);
-    }
-  });
-  // WebSocket chat relay implementation
-  app.get('/ws', (req, res) {
-    return WebSocketSession(
-      onOpen: (ws) {},
-      onClose: (ws) {},
-      onMessage: (ws, dynamic data) async {},
-    );
-  });
-  app.get('*', (req, res) {
-    return Directory(webdir);
-  });
-  await app.listen(serverPort, serverUrl);
+    });
+    // WebSocket chat relay implementation
+    app.get('/ws', (req, res) {
+      return WebSocketSession(
+        onOpen: (ws) {},
+        onClose: (ws) {},
+        onMessage: (ws, dynamic data) async {},
+      );
+    });
+    app.get('*', (req, res) {
+      return Directory(webdir);
+    });
+    await app.listen(serverPort, serverUrl);
+  }
 }
