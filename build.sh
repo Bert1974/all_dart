c0d9716f3f6eeb77297cc192055eb3ca171f7390
Bert Bruggeman <bbruggeman1974@gmail.com>
2023-04-16 19:31:50 +0200

    testing

--- build.sh
+++ build.sh
@@ -5,10 +5,12 @@ dart run build_runner build
 cd ..
 cd server
 dart pub get
+bash <(curl -s https://raw.githubusercontent.com/objectbox/objectbox-go/main/install.sh)
 dart run build_runner build
 dart compile exe bin/server.dart -o ../publish/server.exe
 cp lib/objectbox.* ../publish/
 cp config.json ../publish/
+ls lib
 cd ..
 cd main
 flutter pub get
