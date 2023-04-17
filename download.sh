bdd8e363e53966ace2d81eec0569b0fc2e1bf933
Bert Bruggeman <bertbruggeman@ziggo.nl>
2023-04-17 14:49:20 +0200

    download

---
+++ download.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cLibVersion=0.18.0
+
+SUPPORTED_PLATFORMS=(
+"linux-x64"
+"linux-armv6hf"
+"linux-armv7hf"
+"linux-aarch64"
+"windows-x86"
+"windows-x64"
+"macos-universal")
+
+for platform in "${SUPPORTED_PLATFORMS[@]}"
+do
+	echo $platform
+	if [[ $platform = w* ]]
+	then
+		ext="zip"
+	else
+		ext="tar.gz"
+	fi
+   
+	downloadUrl=https://github.com/objectbox/objectbox-c/releases/download/v${cLibVersion}/objectbox-${platform}.${ext}
+	echo $downloadUrl
+	curl -s --output "objectbox-${platform}.${ext}" $downloadUrl
+done
